ba4a18143e24b2deb245175c09bb5d73da31a72c
Camilo Aguilar <camiloaaguilara@hotmail.com>
2026-01-29 01:32:48 -0500

    Se instala tailwind y se genera la página dashboard

---
+++ src/app/gifs/pages/dashboard-pages/dashboard-pages.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  imports: [],
+  templateUrl: './dashboard-pages.html',
+})
+export default class DashboardPages {
+
+}
