07f84913d636b57d6f1f9f5bbd85f8158502acf2
Camilo Aguilar <camiloaaguilara@hotmail.com>
2026-01-29 01:33:45 -0500

    Se instala tailwind y se genera la página dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-pages/dashboard-pages')
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
